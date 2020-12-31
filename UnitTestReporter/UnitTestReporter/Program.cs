69b2680842e52682e9ec16b6b3dc021bc4c45d1d
Msalpdogan <msalpdogan@gmail.com>
2021-01-01 02:39:49 +0300

    Logger added.

---
+++ UnitTestReporter/UnitTestReporter/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnitTestReporter
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            ///Generate Host Builder and Register the Services for DI
+            var builder = new HostBuilder()
+               .ConfigureServices((hostContext, services) =>
+               {
+                   services.AddScoped<Form1>();
+
+                   //Add Serilog
+                   var serilogLogger = new LoggerConfiguration()
+                           .WriteTo.File(".\\Logs\\Reporter.log", rollingInterval: RollingInterval.Hour)
+                           .CreateLogger();
+                   services.AddLogging(x =>
+                   {
+                       x.SetMinimumLevel(LogLevel.Trace);
+                       x.AddSerilog(logger: serilogLogger, dispose: true);
+                   });
+
+               });
+
+            var host = builder.Build();
+
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+                try
+                {
+                    var form1 = services.GetRequiredService<Form1>();
+                    Application.Run(form1);
+
+                    Console.WriteLine("Success");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error Occured");
+                }
+            }
+        }
+    }
+}

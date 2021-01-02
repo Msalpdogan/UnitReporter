2e43d6fe15fda8a589f6a2da98d8f502a95372ee
Msalpdogan <msalpdogan@gmail.com>
2021-01-02 04:20:55 +0300

    Report Parser added

---
+++ UnitTestReporter/UnitTestReporter.Business/Parser/IParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTestReporter.Core.Models;
+
+namespace UnitTestReporter.Business.Parser
+{
+    public interface IParser
+    {
+        Report Parse(string filePath);
+    }
+}

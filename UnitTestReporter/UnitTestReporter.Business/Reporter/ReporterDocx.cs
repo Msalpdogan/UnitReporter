3de7eb6e20d23280c5343460a91ebc4935d80153
Msalpdogan <msalpdogan@gmail.com>
2021-01-03 14:55:28 +0300

    NUnit Reporter added

--- UnitTestReporter/UnitTestReporter.Business/Reporter/ReporterDocx.cs
+++ UnitTestReporter/UnitTestReporter.Business/Reporter/ReporterDocx.cs
@@ -1,15 +1,67 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using SautinSoft.Document;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using UnitTestReporter.Core.Configuration;
 using UnitTestReporter.Core.Models;
 
 namespace UnitTestReporter.Business.Reporter
 {
     public class ReporterDocx : IReporter<ReporterDocx>
     {
+        private readonly CommonSettings commonSettings;
+        public ReporterDocx(IOptions<CommonSettings> options)
+        {
+            commonSettings = options.Value;
+        }
         public void CreateReport(Report report, string outputPath)
         {
-            throw new NotImplementedException();
+            string templatePath = commonSettings.DocxTemplate;
+            string resultPath = report.FileName + DateTime.Now.ToShortDateString() + ".docx";
+            string isSuccess = "False";
+            string tester = System.Environment.MachineName;
+
+            DocumentCore dc = DocumentCore.Load(templatePath);
+
+            if (report.Failed == 0)
+                isSuccess = "True";
+
+            if (report.TestSuiteList.Count > 0)
+            {
+                foreach (var testSuiteList in report.TestSuiteList)
+                {
+                    foreach (var testMethod in testSuiteList.TestList)
+                    {
+                        var dataSource = new[] {
+                        new {
+                            AssemblyName = report.AssemblyName,
+                            StartTime = report.StartTime,
+                            EndTime = report.EndTime,
+                            Total= report.Total,
+                            Passed=report.Passed,
+                            Failed= report.Failed,
+                            Inconclusive= report.Inconclusive,
+                            Skipped = report.Skipped,
+                            Errors= report.Errors,
+                            TestClassName = testSuiteList.Name,
+                            TestClassStartTime = testSuiteList.StartTime,
+                            TestClassEndTime = testSuiteList.EndTime,
+                            TestMethodName =testMethod.Name,
+                            TestMethodStartTime = testMethod.StartTime,
+                            TestMethodEndTime = testMethod.EndTime,
+                            Result = isSuccess,
+                            Tester = tester
+                        } };
+                        dc.MailMerge.Execute(dataSource);
+                    }
+                }
+            }
+
+            dc.Save(resultPath);
+
+            // Open the result for demonstration purposes.
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(resultPath) { UseShellExecute = true });
         }
     }
 }
